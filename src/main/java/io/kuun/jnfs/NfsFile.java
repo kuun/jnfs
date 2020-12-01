1e9f1f82d935922e79e923817b16e638bf84006a
kuun <kuunwang@gmail.com>
2020-12-01 17:49:35 +0800

    implement class NfsFile

--- src/main/java/io/kuun/jnfs/NfsFile.java
+++ src/main/java/io/kuun/jnfs/NfsFile.java
@@ -1,5 +1,7 @@
 package io.kuun.jnfs;
 
+import io.kuun.jnfs.exceptions.NfsException;
+
 public class NfsFile {
     long filePtr;
     NfsContext context;
@@ -8,43 +10,54 @@ public class NfsFile {
         return null;
     }
 
-    void close() {
-
+    void close() throws NfsException {
+        int ret = NativeNfsContext.close(context.contextPtr, filePtr);
+        failThenThrow(ret);
     }
 
     int pread(long offset, long count, byte[] buf) {
-        return 0;
+        return NativeNfsContext.pread(context.contextPtr, filePtr, offset, count, buf);
     }
 
     int read(NfsFile file, long offset, long count, byte[] buf) {
-        return 0;
+        return NativeNfsContext.read(context.contextPtr, filePtr, count, buf);
     }
 
-    int pwrite(NfsFile file, long count, byte[] buf) {
-        return 0;
+    int pwrite(long offset, long count, byte[] buf) {
+        return NativeNfsContext.pwrite(context.contextPtr, filePtr, offset, count, buf);
     }
 
-    int lseek(NfsFile file, long offset, int whence) {
-        return 0;
+    int write(long count, byte[] buf) {
+        return NativeNfsContext.write(context.contextPtr, filePtr, count, buf);
     }
 
-    void lock(NfsFile file, int lockOp, long count) {
-
+    long lseek(long offset, int whence) {
+        return NativeNfsContext.lseek(context.contextPtr, filePtr, offset, whence);
     }
 
-    void sync(NfsFile file) {
-
+    void sync() {
+        NativeNfsContext.fsync(context.contextPtr, filePtr);
     }
 
-    void truncate(long length) {
-
+    void truncate(long length) throws NfsException {
+        int ret = NativeNfsContext.ftruncate(context.contextPtr, filePtr, length);
+        failThenThrow(ret);
     }
 
-    void chmod(int mode) {
-
+    void chmod(int mode) throws NfsException {
+        int ret = NativeNfsContext.fchmod(context.contextPtr, filePtr, mode);
+        failThenThrow(ret);
     }
 
-    void chown(int uid, int gid) {
+    void chown(int uid, int gid) throws NfsException {
+        int ret = NativeNfsContext.fchown(context.contextPtr, filePtr, uid, gid);
+        failThenThrow(ret);
+    }
 
+    private void failThenThrow(int errno) throws NfsException {
+        if (errno != 0) {
+            String err = NativeNfsContext.getError(context.contextPtr);
+            throw new NfsException(errno, err);
+        }
     }
 }
